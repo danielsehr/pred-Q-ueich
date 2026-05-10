229827f515856b0771357fa54a39423cb54d2467
danielsehr <daniel.serr7028@gmail.com>
2026-05-11 00:03:39 +0200

    add: changed relative to absolute path to database

--- database/db.py
+++ database/db.py
@@ -1,7 +1,13 @@
+from pathlib import Path
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///queich.db"
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASE_PATH = BASE_DIR / "queich.db"
+
+DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
 
 engine = create_engine(DATABASE_URL)
 
