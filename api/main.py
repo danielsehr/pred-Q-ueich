82944aaf685964d1fd3b58eb3980379cd7e692cc
danielsehr <daniel.serr7028@gmail.com>
2026-05-12 00:44:24 +0200

    changed: modified so it fits the discharge data

--- api/main.py
+++ api/main.py
@@ -1,23 +1,28 @@
 from fastapi import FastAPI
 
 from database.db import SessionLocal 
-from database.models import Forecast
+from database.models import Discharge, Forecast
 
 
+# Create web application instance -> main backend server object
 app = FastAPI()
 
+# Decorator -> when sb. GET requests to /forecast -> run function:
+# When streamlit do requests.get() -> get_forecast() is executed.
 @app.get("/forecast")
 
 def get_forecast():
+    # Create live DB session / connection
     session = SessionLocal()
     
-    rows = session.query(Forecast).all()
+    # Query forecast table in SQL for all rows -> SELECT * FROM forecast
+    rows = session.query(Discharge).all()
     
+    # Return row objects in list of dicts, FastAPI directly dumps it into json http response
     return [
         {
             "timestamp": r.timestamp.isoformat(),
-            "observed": r.observed,
-            "predicted": r.predicted,
+            "discharge": r.discharge,
         }
         for r in rows
     ]
