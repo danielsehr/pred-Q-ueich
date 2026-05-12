0c837fa11a05f8ba30d41d5bad58976a8de15dd1
danielsehr <daniel.serr7028@gmail.com>
2026-05-12 15:19:42 +0200

    changed: outsourced configuration links to config file

---
+++ jobs/config.py
@@ -0,0 +1,15 @@
+URL = (
+    "https://geodaten-wasser.rlp-umwelt.de/"
+    "api/data/messstellen_wasserstand_abflusswerte_30"
+    "?w=messstellennummer=2377050700"
+)
+
+HEADERS  = {
+    "User-Agent": (
+        "Mozilla/5.0 (X11; Linux x86_64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/120.0 Safari/537.36"
+    ),
+    "Accept": "application/json, text/plain, */*",
+    "Referer": "https://geodaten-wasser.rlp-umwelt.de/",
+}
