80cb43fa7368d722435db8627210fa15ba2c2c1f
danielsehr <daniel.serr7028@gmail.com>
2026-05-13 00:54:56 +0200

    add: set index to see timestamp in plot

--- dashboard/app.py
+++ dashboard/app.py
@@ -10,8 +10,12 @@ response = requests.get(API_URL)
 data = response.json()
 
 df = pd.DataFrame(data)
+df = df.set_index(keys=["timestamp"])
+print(df)
 
 
 #--- Dashboard ---
 st.title("Queich Forecast Dashboard")
-st.line_chart(df[["discharge"]])
+st.line_chart(
+    data=df[["discharge"]],
+    )
