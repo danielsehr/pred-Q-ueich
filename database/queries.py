dc3e9e3e3879a7e3ea789b8fc9eb3bce202360f2
danielsehr <daniel.serr7028@gmail.com>
2026-05-13 16:24:54 +0200

    add: function to load inference data from db

--- database/queries.py
+++ database/queries.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sqlalchemy import select
 
 from database.db import SessionLocal
-from database.models import Discharge
+from database.models import Discharge, Inference
 
 
 def load_discharge_data() -> pd.DataFrame:
@@ -28,8 +28,40 @@ def load_discharge_data() -> pd.DataFrame:
         )
         
         if not df.empty:
-            df = df.set_index("timestamp")
+            df = df.set_index(keys="timestamp")
             
+        return df
+    
+    finally:
+        session.close()
+        
+
+
+def load_inference_data() -> pd.DataFrame:
+    session = SessionLocal()
+    
+    try:
+        statement = (
+            select(Inference)
+            .order_by(Inference.timestamp)
+        )
+        
+        rows = session.execute(statement=statement).scalars().all()
+        
+        df = pd.DataFrame(
+            [
+                {
+                    "timestamp": r.timestamp,
+                    "observed": r.observed,
+                    "predicted": r.predicted,
+                    "model_version": r.model_version
+                }
+                for r in rows
+            ]
+        )
+        
+        if not df.empty:
+            df = df.set_index(keys="timestamp")
         
         return df
     
