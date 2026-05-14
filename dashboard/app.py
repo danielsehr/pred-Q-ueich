d11bbf11868c6ff88600f939eac421c085f80d21
danielsehr <daniel.serr7028@gmail.com>
2026-05-14 16:55:54 +0200

    add: cutoff for displaying 1 day intervall of discharge data only, added plotly for graph

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,21 +1,62 @@
 import requests
 import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
 
 
 API_URL = "http://localhost:8000/forecast"
 
 response = requests.get(API_URL)
+print("response:", response)
 
 data = response.json()
 
-df = pd.DataFrame(data)
-df = df.set_index(keys=["timestamp"])
-print(df)
+
+df_discharge = pd.DataFrame(data["discharge"])
+df_inference = pd.DataFrame(data["inference"])
+
+df_discharge = df_discharge.set_index(keys=["timestamp"])
+df_discharge.index = pd.to_datetime(df_discharge.index)
+
+cutoff = pd.Timestamp.now() - pd.Timedelta(days=1)
+
+df_discharge = (
+    df_discharge[df_discharge.index >= cutoff]
+)
+
+df_inference = df_inference.set_index(keys=["timestamp"])
+
 
 
 #--- Dashboard ---
-st.title("Queich Forecast Dashboard")
-st.line_chart(
-    data=df[["discharge"]],
+fig = go.Figure()
+
+# historical
+fig.add_trace(
+    go.Scatter(
+        x=df_discharge.index,
+        y=df_discharge["discharge"],
+        mode="lines",
+        name="Observed",
     )
+)
+
+# forecast line + points
+fig.add_trace(
+    go.Scatter(
+        x=df_inference.index,
+        y=df_inference["predicted"],
+        mode="lines+markers",
+        name="Forecast",
+        line=dict(dash="dash"),
+    )
+)
+
+fig.update_layout(
+    xaxis_title="Time",
+    yaxis_title="Discharge",
+    legend=dict(orientation="h"),
+    template="plotly_white",
+)
+
+st.plotly_chart(fig, use_container_width=True)
