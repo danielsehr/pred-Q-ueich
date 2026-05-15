2074d768470221853716f6b6c4311905254dbda5
danielsehr <daniel.serr7028@gmail.com>
2026-05-16 01:09:34 +0200

    add: shift, sum vars and lags, inference_steps

--- configs/model_config.py
+++ configs/model_config.py
@@ -1,3 +1,4 @@
+# --- build_model.py ---
 hparams = {
     "n_estimators": 10000,
     "max_depth": 3,
@@ -8,3 +9,26 @@ hparams = {
     "random_state":42,
     "early_stopping_rounds": 50
 }
+
+
+# --- features.py ---
+shift_vars = [
+    "pr", "temp", "pet", "discharge"
+    ]
+
+sum_vars = [
+    "pr", "discharge"
+    ]
+
+# Lags in lag * timestamp (15 min)
+shift_lags = [
+    1, 2, 4, 8, 12, 24, 48, 96
+    ]
+
+# Lags in days
+sum_lags = [
+    3, 7, 14, 21, 28, 35, 42, 56
+    ]
+
+# Timesteps of inference. 1 -> 15 min
+inference_steps = 1
