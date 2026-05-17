8689197450aa35fec37379b43dd9bab21e776b28
danielsehr <daniel.serr7028@gmail.com>
2026-05-18 01:03:22 +0200

    add: functions for clipping and extractin precip data from xarray

---
+++ jobs/icon/process.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import pandas as pd
+import geopandas as gpd
+import xarray as xr
+
+from configs.jobs_config import DECOMPRESSED_DIR
+from utils.logger import logger
+
+
+def clip_to_catchment(
+    dataset: xr.Dataset,
+    catchment: gpd.GeoDataFrame,
+    crs: str
+    ) -> xr.Dataset:
+    
+    if "tp" in dataset:
+        precip = dataset["tp"]
+    
+    # Check CRS
+    precip = precip.rio.write_crs(crs)
+    assert precip.rio.crs == catchment.crs
+    
+    
+    # --- Crop bounding box of catchment ---
+    minx, miny, maxx, maxy = catchment.total_bounds
+
+    precip_crop = precip.rio.clip_box(
+        minx=minx,
+        miny=miny,
+        maxx=maxx,
+        maxy=maxy,
+    )
+    
+    # # --- Optional geometry clip ---
+    # precip_masked = precip_cropped.rio.clip(
+    #     catchment.geometry,
+    #     catchment.crs,
+    #     drop=False,
+    # )
+    
+    return precip_crop  
