3fff0c0e2a5d98604095afbdca326c563f9b2609
danielsehr <daniel.serr7028@gmail.com>
2026-05-23 19:58:46 +0200

    new: function for query icon precip forecast data

--- database/queries.py
+++ database/queries.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from utils.logger import logger
 from database.db import SessionLocal
-from database.models import Discharge, Inference
+from database.models import Discharge, Inference, IconPrecipForecast
 
 
 def load_discharge_data() -> pd.DataFrame:
@@ -77,5 +77,43 @@ def load_inference_data() -> pd.DataFrame:
         raise
     
     
+    finally:
+        session.close()
+        
+        
+
+def load_precip_forecast_data() -> pd.DataFrame:
+    session = SessionLocal()
+    
+    try:
+        statement = (
+            select(IconPrecipForecast)
+            .order_by(IconPrecipForecast.timestamp)
+        )
+        
+        rows = session.execute(statement=statement).scalars().all()
+        
+        df = pd.DataFrame(
+            [
+                {
+                    "timestamp": r.timestamp,
+                    "precip_mean": r.precip_mean,
+                    # "model_version": r.model_version
+                }
+                for r in rows
+            ]
+        )
+        
+        if not df.empty:
+            df = df.set_index(keys="timestamp")
+            
+        return df
+    
+    
+    except SQLAlchemyError:
+        logger.exception("Failed to load precip mean forecast data")
+        raise
+        
+        
     finally:
         session.close()
