b204a7b2893d77d2c3753e8156894e5e17003722
danielsehr <daniel.serr7028@gmail.com>
2026-05-23 19:29:42 +0200

    add: icon precip forecast ingestion

--- configs/scheduler_config.py
+++ configs/scheduler_config.py
@@ -1,5 +1,6 @@
 from jobs.ingest_discharge import main as ingest_discharge
 from jobs.ingest_inference import main as ingest_inference
+from jobs.icon.ingest_icon import main as ingest_precip_forecast
 
 
 JOBS = [
@@ -11,6 +12,11 @@ JOBS = [
     {
         "name": "ingest_inference",
         "func": ingest_inference,
+        "interval_minutes": 10,
+    },
+    {
+        "name": "ingest_precip_forecast",
+        "func": ingest_precip_forecast,
         "interval_minutes": 10,
     },
 ]
