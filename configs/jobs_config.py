44f60839675b96f35314b31f87723c2807cc1d48
danielsehr <daniel.serr7028@gmail.com>
2026-05-23 18:43:24 +0200

    add: clip crs macro

--- configs/jobs_config.py
+++ configs/jobs_config.py
@@ -26,3 +26,5 @@ DECOMPRESSED_DIR = "data/forecast_precip/icon_d2/decompressed"
 
 # --- process.py ---
 CATCHMENT_PATH = "data/catchment/catchment_queich_siebeldingen.gpkg"
+
+CLIP_CRS = "EPSG: 4326"
