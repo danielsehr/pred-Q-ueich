4553c0a3224b66e8e5d96c7fa027de231ffccb54
danielsehr <daniel.serr7028@gmail.com>
2026-05-25 00:15:19 +0200

    new: script reading and processing radolan data from decompressed files

---
+++ jobs/radolan/process_radolan.py
@@ -0,0 +1,119 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+import wradlib as wrl
+import xarray as xr
+
+from utils.logger import logger
+
+
+
+def read_radolan_data(
+    file_path: str | Path
+    ) -> tuple[np.ndarray, dict]:
+    
+    file_path = Path(file_path)
+    
+    data, attrs = wrl.io.read_radolan_composite(file_path)
+    
+    data = np.array(data).astype(float)
+    data = np.where(data == -9999, np.nan, data)
+    
+    return data, attrs
+
+
+def extract_radolan_timestamp(
+    file_path: str | Path
+    ) -> pd.Timestamp:
+    
+    stem = Path(file_path).stem
+    
+    _, back = stem.split("_")
+    _, timestring, _ = back.split("-", maxsplit=2)
+    
+    observation_time = pd.to_datetime(timestring, format="%y%m%d%H%M")
+    
+    return observation_time
+
+
+def extract_radolan_coords(
+    attrs: dict
+    ) -> tuple[np.ndarray, np.ndarray]:
+    
+    grid = wrl.georef.get_radolan_grid(
+        attrs["nrow"],
+        attrs["ncol"]
+    )
+    
+    x2d = np.array(grid)[:, :, 0]
+    y2d = np.array(grid)[:, :, 1]
+    
+    return x2d, y2d
+
+
+def radolan_to_xarray(
+    file_path: str | Path
+    ) -> xr.DataArray:
+    
+    data, attrs = read_radolan_data(file_path=file_path)
+    
+    observation_time = extract_radolan_timestamp(file_path=file_path)
+    
+    x2d, y2d = extract_radolan_coords(attrs=attrs)
+    
+    
+    data_xr = xr.DataArray(
+        data[np.newaxis, :, :],
+        dims=("time", "y", "x"),
+        coords={
+            "time": [observation_time],
+            "x": x2d[0, :],
+            "y": y2d[:, 0],
+            },
+        name="precip"
+        )
+    
+    data_xr = (
+    data_xr
+    .rio.set_spatial_dims(x_dim="x", y_dim="y")
+    .rio.write_crs(wrl.georef.projection.create_crs("dwd-radolan"))
+)  
+    # data_xr = data_xr.rio.write_crs(
+    #     wrl.georef.projection.create_crs("dwd-radolan")
+    # )
+
+    return data_xr
+
+
+def clip_to_catchment(
+    dataset: xr.DataArray,
+    catchment: gpd.GeoDataFrame,
+    crs: str
+    ) -> xr.Dataset:
+    
+    precip = dataset
+    
+    # Check CRS
+    catchment = catchment.to_crs(crs=crs)
+    assert precip.rio.crs == catchment.crs
+    
+     # --- Crop bounding box of catchment ---
+    minx, miny, maxx, maxy = catchment.total_bounds
+
+    precip_crop = precip.rio.clip_box(
+        minx=minx,
+        miny=miny,
+        maxx=maxx,
+        maxy=maxy,
+    )
+    
+    # # --- Optional geometry clip ---
+    # precip_masked = precip_cropped.rio.clip(
+    #     catchment.geometry,
+    #     catchment.crs,
+    #     drop=False,
+    # )
+    
+    return precip_crop  
