415430f8b3cba8ffc4219c23ab173d2fb61a8fb7
danielsehr <daniel.serr7028@gmail.com>
2026-05-25 18:40:50 +0200

    add: precip obs and pred to chart

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from prepare_data import prepare_data
 from configs.dashboard_config import config
@@ -30,13 +31,38 @@ except requests.RequestException as e:
 # --- Prepare data for plotting ---
 df_discharge = pd.DataFrame(data["discharge"])
 df_inference = pd.DataFrame(data["inference"])
+df_precip_obs = pd.DataFrame(data["precip_obs"])
+df_precip_pred = pd.DataFrame(data["precip_pred"])
 
-df_discharge = prepare_data(df_discharge, days=1)
-df_inference = prepare_data(df_inference, days=1)
+days=14
+df_discharge = prepare_data(df_discharge, days=days)
+df_inference = prepare_data(df_inference, days=days)
+df_precip_obs = prepare_data(df_precip_obs, days=days)
+df_precip_pred = prepare_data(df_precip_pred, days=days)
 
 
 #--- Dashboard ---
-fig = go.Figure()
+fig  = make_subplots(
+    rows=2,
+    cols=1,
+    shared_xaxes=True,
+    vertical_spacing=0.1,
+    row_heights=[0.25, 0.75],
+)
+
+
+# Observed Precipitation
+fig.add_trace(
+    go.Bar(
+        x=df_precip_obs.index,
+        y=df_precip_obs["precip_mean"],
+        name="Observed Precipitation",
+        opacity=0.7,
+    ),
+    row=1,
+    col=1,
+)
+
 
 # historical
 fig.add_trace(
@@ -44,8 +70,10 @@ fig.add_trace(
         x=df_discharge.index,
         y=df_discharge["discharge"],
         mode="lines+markers",
-        name="Observed",
-    )
+        name="Observed Discharge",
+    ),
+    row=2,
+    col=1,
 )
 
 # forecast line + points
@@ -54,9 +82,11 @@ fig.add_trace(
         x=df_inference.index,
         y=df_inference["predicted"],
         mode="lines+markers",
-        name="Forecast",
+        name="Forecast Discharge",
         line=dict(dash="dash"),
-    )
+    ), 
+    row=2,
+    col=1,
 )
 
 fig.update_layout(
@@ -66,4 +96,12 @@ fig.update_layout(
     template="plotly_white",
 )
 
-st.plotly_chart(fig)
+fig.update_yaxes(title_text="Precipitation", row=1, col=1)
+fig.update_yaxes(title_text="Discharge", row=2, col=1)
+fig.update_xaxes(title_text="Time", row=2, col=1)
+
+
+st.set_page_config(layout="wide")
+fig.update_layout(height=850)
+
+st.plotly_chart(fig, use_container_width=True)
