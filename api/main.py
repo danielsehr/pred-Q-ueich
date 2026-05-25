44b5c9e60e612c0b9e2a2bff42827da5120eee41
danielsehr <daniel.serr7028@gmail.com>
2026-05-25 18:40:19 +0200

    add: precip obs and pred query

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from database.db import SessionLocal 
 from database.models import Discharge, Inference
-from database.queries import load_discharge_data, load_inference_data
+from database.queries import load_discharge_data, load_inference_data, load_precip_observation_data, load_precip_forecast_data
 
 
 # Create web application instance -> main backend server object
@@ -15,9 +15,13 @@ app = FastAPI()
 def get_forecast():
     df_discharge = load_discharge_data()
     df_inference = load_inference_data()
+    df_precip_obs = load_precip_observation_data()
+    df_precip_pred = load_precip_forecast_data()
     
     return {
         "discharge": df_discharge.reset_index().to_dict(orient="records"),
-        "inference": df_inference.reset_index().to_dict(orient="records")
+        "inference": df_inference.reset_index().to_dict(orient="records"),
+        "precip_obs": df_precip_obs.reset_index().to_dict(orient="records"),
+        "precip_pred": df_precip_pred.reset_index().to_dict(orient="records"),
     }
         
