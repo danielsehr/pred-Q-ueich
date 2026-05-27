0a7de012848dfe49940fe44cd32437ac27d201bb
danielsehr <daniel.serr7028@gmail.com>
2026-05-27 15:58:01 +0200

    changed: mv ingest inference job to top and set interval to 5

--- configs/scheduler_config.py
+++ configs/scheduler_config.py
@@ -1,18 +1,18 @@
-from jobs.ingest_discharge import main as ingest_discharge
-from jobs.ingest_inference import main as ingest_inference
+from jobs.discharge.ingest_discharge import main as ingest_discharge
+from jobs.inference.ingest_inference import main as ingest_inference
 from jobs.icon.ingest_icon import main as ingest_precip_forecast
 from jobs.radolan.ingest_radolan import main as ingest_precip_observation
 
 
 JOBS = [
     {
-        "name": "ingest_discharge",
-        "func": ingest_discharge,
-        "interval_minutes": 10,
-    },
-    {
         "name": "ingest_inference",
         "func": ingest_inference,
+        "interval_minutes": 5,
+    },
+    {
+        "name": "ingest_discharge",
+        "func": ingest_discharge,
         "interval_minutes": 10,
     },
     {
