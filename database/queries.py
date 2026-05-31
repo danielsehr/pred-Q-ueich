901f34a4a73e529c98a1af35d83e6b418369a853
danielsehr <daniel.serr7028@gmail.com>
2026-06-01 00:45:23 +0200

    add: fncts for querying observation or forecast data

--- database/queries.py
+++ database/queries.py
@@ -9,9 +9,7 @@ from database.db import SessionLocal
 from database.models import Discharge, Inference, IconPrecipForecast, RadolanPrecipObservation
 
 
-
-
-def load_timeseries(
+def load_observation_timeseries(
     model,
     value_column: str,
     output_name: str,
@@ -49,13 +47,52 @@ def load_timeseries(
         
     return df
 
+
+def load_forecast_timeseries(
+    model,
+    value_column: str,
+    output_name: str,
+    days: int,
+    ) -> pd.DataFrame:
+    
+    start = pd.Timestamp.now() - pd.Timedelta(days=days)
+    end = start + pd.Timedelta(days=days)
+
+    
+    column = getattr(model, value_column)
+    
+    with SessionLocal() as session:
+        statement = (
+            select(model.timestamp, column)
+            .where(model.timestamp > start)
+            .where(model.timestamp <= end)
+            .order_by(model.timestamp)
+        )
+
+        rows = session.execute(statement).all()
+        
+        
+    df = pd.DataFrame(
+        [
+            {
+                "timestamp": ts,
+                output_name: value
+            } for ts, value in rows
+        ] 
+    )
+    
+    if not df.empty:
+        df = df.set_index("timestamp")
+        
+    return df
+
     
 
 def load_discharge_data(
     days: int = QUERY_TIMEDELTA_DAYS
     ) -> pd.DataFrame:
     
-    df = load_timeseries(
+    df = load_observation_timeseries(
         model=Discharge, 
         value_column="discharge",
         output_name="discharge",
@@ -69,7 +106,7 @@ def load_inference_data(
     days: int = QUERY_TIMEDELTA_DAYS
     ) -> pd.DataFrame:
     
-    df = load_timeseries(
+    df = load_forecast_timeseries(
         model=Inference, 
         value_column="predicted",
         output_name="predicted",
@@ -83,7 +120,7 @@ def load_precip_forecast_data(
     days: int = QUERY_TIMEDELTA_DAYS
     ) -> pd.DataFrame:
     
-    df = load_timeseries(
+    df = load_forecast_timeseries(
         model=IconPrecipForecast, 
         value_column="precip_mean",
         output_name="precip_mean",
@@ -97,7 +134,7 @@ def load_precip_observation_data(
     days: int = QUERY_TIMEDELTA_DAYS
     ) -> pd.DataFrame:
     
-    df = load_timeseries(
+    df = load_observation_timeseries(
         model=RadolanPrecipObservation, 
         value_column="precip_mean",
         output_name="precip_mean",
