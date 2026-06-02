0d001a37eaf5d78859b345860166171ba8a416fd
danielsehr <daniel.serr7028@gmail.com>
2026-06-02 23:49:15 +0200

    changed: separation of config and method, moved method in jobs script for schedueler

---
+++ scheduler/jobs.py
@@ -0,0 +1,10 @@
+from jobs.discharge.ingest_discharge import main as ingest_discharge
+from jobs.inference.ingest_inference import main as ingest_inference
+
+
+def ingest_discharge_and_inference() -> None:
+
+    inserted = ingest_discharge()
+
+    if inserted:
+        ingest_inference()
