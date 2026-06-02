25884d9924c38f2d872f4d17c2a46898643c093c
danielsehr <daniel.serr7028@gmail.com>
2026-06-02 23:49:56 +0200

    add: registry file for scheduler jobs

---
+++ scheduler/registry.py
@@ -0,0 +1,10 @@
+from jobs.icon.ingest_icon import main as ingest_precip_forecast
+from jobs.radolan.ingest_radolan import main as ingest_precip_observation
+
+from scheduler.jobs import ingest_discharge_and_inference
+
+JOB_REGISTRY = {
+    "ingest_discharge_and_inference": ingest_discharge_and_inference,
+    "ingest_precip_forecast": ingest_precip_forecast,
+    "ingest_precip_observation": ingest_precip_observation,
+}
