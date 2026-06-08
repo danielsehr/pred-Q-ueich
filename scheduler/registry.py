a39fc6f4a736a9f93ed7663d716b9b5014bb43d8
danielsehr <daniel.serr7028@gmail.com>
2026-06-09 00:21:39 +0200

    add: hourly radolan obs

--- scheduler/registry.py
+++ scheduler/registry.py
@@ -1,10 +1,13 @@
 from jobs.icon.ingest_icon import main as ingest_precip_forecast
 from jobs.radolan.ingest_radolan import main as ingest_precip_observation
+from jobs.radolan_hourly.ingest_radolan import main as ingest_precip_hourly_observation
 
 from scheduler.jobs import ingest_discharge_and_inference
+
 
 JOB_REGISTRY = {
     "ingest_discharge_and_inference": ingest_discharge_and_inference,
     "ingest_precip_forecast": ingest_precip_forecast,
     "ingest_precip_observation": ingest_precip_observation,
+    "ingest_precip_hourly_observation": ingest_precip_hourly_observation
 }
