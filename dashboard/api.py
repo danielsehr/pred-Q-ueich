8cf9937daae2bc59f34bb1b360b4e134770cd7b5
danielsehr <daniel.serr7028@gmail.com>
2026-06-09 11:10:47 +0200

    add: spacing for readability

--- dashboard/api.py
+++ dashboard/api.py
@@ -6,14 +6,17 @@ from configs import settings
 
 @st.cache_data(ttl=600)
 def fetch_dashboard_data() -> dict:
+    
     try:
         response = requests.get(
             url=settings.dashboard.api_url,
             timeout=10
             )
+        
         response.raise_for_status()
 
         return response.json()
+
 
     except requests.RequestException as e:
         st.error(f"API unavailable: {e}")
