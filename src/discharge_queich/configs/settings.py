95434bb7d2d266099f43176e0f3fd6dc82362b7e
danielsehr <daniel.serr7028@gmail.com>
2026-06-24 11:39:38 +0200

    add: SettingsConfigDict for parsing env variables injected from docker, prepared script for final custom settingsource solution

--- src/discharge_queich/configs/settings.py
+++ src/discharge_queich/configs/settings.py
@@ -1,5 +1,12 @@
 from pathlib import Path
+import yaml
 from pydantic import BaseModel
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+    PydanticBaseSettingsSource,
+    YamlConfigSettingsSource
+    )
 
 from discharge_queich.configs.database import DatabaseSettings
 from discharge_queich.configs.dashboard import DashboardSettings
@@ -8,9 +15,15 @@ from discharge_queich.configs.model import ModelSettings
 from discharge_queich.configs.scheduler import SchedulerSettings
 
 
-class Settings(BaseModel):
+class Settings(BaseSettings):
+    
     database: DatabaseSettings
     dashboard: DashboardSettings
     ingestion: IngestionSettings
     model: ModelSettings
     scheduler: SchedulerSettings
+    
+    model_config = SettingsConfigDict(
+        env_nested_delimiter="__",
+        extra="ignore"
+    )
