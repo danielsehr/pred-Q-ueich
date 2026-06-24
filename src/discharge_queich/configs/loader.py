03559827995517b5aaabfac8771cdffc5c06ae8a
danielsehr <daniel.serr7028@gmail.com>
2026-06-24 11:37:56 +0200

    add: Path type to load_yaml, load_settings automatically replaces env api url variable in config for docker setup, temporary solution

--- src/discharge_queich/configs/loader.py
+++ src/discharge_queich/configs/loader.py
@@ -1,38 +1,34 @@
 from pathlib import Path
-
 import yaml
+import os
 
 from discharge_queich.configs.settings import Settings
-from discharge_queich.configs.database import DatabaseSettings
-from discharge_queich.configs.dashboard import DashboardSettings
-from discharge_queich.configs.ingestion import IngestionSettings
-from discharge_queich.configs.model import ModelSettings
-from discharge_queich.configs.scheduler import SchedulerSettings
 
 
 CONFIG_DIR = Path(__file__).parent / "yaml"
 
-def load_yaml(filename: str) -> dict:
-    with open(CONFIG_DIR / filename, "r") as f:
+def load_yaml(filename: str | Path) -> dict:
+    with open(Path(CONFIG_DIR) / filename, "r") as f:
         return yaml.safe_load(f)
-    
+
+
     
 def load_settings() -> Settings:
+
+    yaml_config = {
+        "database": load_yaml("database.yaml"),
+        "dashboard": load_yaml("dashboard.yaml"),
+        "ingestion": load_yaml("ingestion.yaml"),
+        "model": load_yaml("model.yaml"),
+        "scheduler": load_yaml("scheduler.yaml"),
+    }
     
-    return Settings(
-        database=DatabaseSettings.model_validate(
-            load_yaml("database.yaml")
-        ),
-        dashboard=DashboardSettings.model_validate(
-            load_yaml("dashboard.yaml")
-        ),
-        ingestion=IngestionSettings.model_validate(
-            load_yaml("ingestion.yaml")
-        ),
-        model=ModelSettings.model_validate(
-            load_yaml("model.yaml")    
-        ),
-        scheduler=SchedulerSettings.model_validate(
-            load_yaml("scheduler.yaml")
+    yaml_config["dashboard"]["api_url"] = (
+    os.getenv(
+        key="DASHBOARD__API_URL",
+        default=yaml_config["dashboard"]["api_url"]
         )
     )
+
+    # return Settings.model_validate(yaml_config)
+    return Settings(**yaml_config)
